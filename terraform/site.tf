38e4997ce59fb5e322ea048f703763134d917d55
John Jarvis <john@jarv.org>
2017-05-28 17:18:15 +0200

    Adding step to create key symlinks.

--- terraform/site.tf
+++ terraform/site.tf
@@ -20,7 +20,11 @@ resource "null_resource" "pre_archive" {
   provisioner "local-exec" {
     command = "${path.root}/../bin/create-client-keys"
   }
-
+  # Ensure lambda func symlinks point to the keys in the
+  # private dir.
+  provisioner "local-exec" {
+    command = "${path.root}/../bin/create-key-symlinks"
+  }
   # Stage files that are shared and keys needed for the lambda function
   provisioner "local-exec" {
     command = "cp ${path.root}/../cmdchallenge/bin/challenge.py ${path.root}/../lambda_src/runcmd/challenge.py"
